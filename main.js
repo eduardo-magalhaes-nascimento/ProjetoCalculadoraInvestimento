8feb0159594ae510977c392715f6eb2fc3d478f1
Eduado Henrique <eduardohenri377@gmail.com>
2025-12-08 21:54:30 -0400

    Adicionando validações nos inputs atravez da tag name

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ const form = document.getElementById("investment-form");
 
 function renderProgression(evt) {
   evt.preventDefault();
-//   const startingAmount = form['starting-amount']
+  //   const startingAmount = form['starting-amount']
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -27,6 +31,31 @@ function renderProgression(evt) {
   );
 
   console.log(returnsArray);
+}
+
+function validateInput(evt) {
+  if(evt.target.value === '') {
+    return;
+  }
+
+  const { parentElement } = evt.target; // Tag pai tem que ser pintada
+  const grandParentElement = evt.target.parentElement.parentElement; // Tag avo tem que ser adicionada a altura
+  const inputValue = evt.target.value.replace(',','.');
+  if(isNaN(inputValue) || Number(inputValue) <= 0){
+    const errorTextElement = document.createElement('p'); //<p></p>
+    errorTextElement.classList.add('text-red-500'); //<p class="text-red-500"></p>
+    errorTextElement.innerText = "Insira um valor númerico e maior que zero";
+
+    parentElement.classList.add('error');
+    grandParentElement.appendChild(errorTextElement)
+  }
+
+}
+
+for (const formElement of form) {
+  if(formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+    formElement.addEventListener('blur', validateInput);
+  }
 }
 
 form.addEventListener("submit", renderProgression);
